712e088bba4ba891a8dc2b69db71069e721a500b
Stuart <stuart@Stuarts-MacBook-Air.local>
2020-04-09 20:40:25 -0500

    Initial commit

---
+++ HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Stuart on 4/9/20.
+//  Copyright © 2020 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
